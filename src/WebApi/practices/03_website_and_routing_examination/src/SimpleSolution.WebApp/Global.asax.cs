bbdeeb8f9fb0b79ef1885b3ffb83be5165dcdc05
liwei <weili@thoughtworks.com>
2017-07-07 19:11:12 +0800

    merge

---
+++ src/WebApi/practices/03_website_and_routing_examination/src/SimpleSolution.WebApp/Global.asax.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Web.Http;
+
+namespace SimpleSolution.WebApp
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            Bootstrapper.Init(GlobalConfiguration.Configuration);
+        }
+    }
+}
---
+++ src/WebApi/practices/03_website_and_routing_examination/src/SimpleSolution.WebApp/controller/UserController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace SimpleSolution.WebApp.controller
+{
+    public class UserController : ApiController
+    {
+        public HttpResponseMessage GetById(int id)
+        {
+            return Request.Text(HttpStatusCode.OK, $"get user by id({id})");
+        }
+    }
+}
