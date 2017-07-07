d5f45eaf965e665653c4cf62a82ee12022d5e741
liwei <weili@thoughtworks.com>
2017-07-07 19:11:45 +0800

    finish local 1

--- src/LocalApi/01_invoke_controller_action/src/LocalApi/ControllerActionInvoker.cs
+++ src/LocalApi/01_invoke_controller_action/src/LocalApi/ControllerActionInvoker.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Linq;
+using System.Net;
 using System.Net.Http;
+using static System.String;
 
 namespace LocalApi
 {
@@ -7,7 +10,25 @@ namespace LocalApi
     {
         public static HttpResponseMessage InvokeAction(ActionDescriptor actionDescriptor)
         {
-            throw new NotImplementedException();
+            var controller = actionDescriptor.Controller;
+            var actionName = actionDescriptor.ActionName;
+            Type type = controller.GetType();
+            var instance = Activator.CreateInstance(type);
+            var action = type.GetMethods().SingleOrDefault(m => String.Equals(m.Name, actionName, StringComparison.CurrentCultureIgnoreCase));
+
+            if (instance == null || action == null)
+            {
+                return new HttpResponseMessage(HttpStatusCode.NotFound);
+            }
+
+            try
+            {
+                return (HttpResponseMessage) action.Invoke(instance, new object[] { });
+            }
+            catch
+            {
+                return new HttpResponseMessage(HttpStatusCode.InternalServerError);
+            }
         }
     }
 }
