dd5e30241e2a81374b419b6a22f694c89ccb02ab
Li Weii <weili@thoughtworks.com>
2017-07-19 21:35:39 +0800

    finish munual fac 03

--- src/Manualfac/03_should_create_type_reflectively/src/Manualfac/Activators/ReflectiveActivator.cs
+++ src/Manualfac/03_should_create_type_reflectively/src/Manualfac/Activators/ReflectiveActivator.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Linq;
+using System.Reflection;
+using Manualfac.Services;
 
 namespace Manualfac.Activators
 {
@@ -22,7 +25,15 @@ namespace Manualfac.Activators
 
         public object Activate(IComponentContext componentContext)
         {
-            throw new NotImplementedException();
+            var constructors = serviceType.GetConstructors();
+            if (constructors.Length != 1)
+            {
+                throw new DependencyResolutionException();
+            }
+            var constructor = constructors.First();
+            ParameterInfo[] parametesTypess = constructor.GetParameters();
+            var parameters = parametesTypess.Select(p => componentContext.ResolveComponent(new TypedService(p.ParameterType))).ToArray();
+            return Activator.CreateInstance(serviceType, parameters);
         }
 
         #endregion
