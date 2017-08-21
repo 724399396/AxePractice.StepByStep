02fa61250ae26e2c048a51bd695516697c88ef79
liwei <weili@thoughtworks.com>
2017-08-21 09:37:33 +0800

    second version for practice uac

--- src/WebApi/practices/04_http_message_handling/src/SampleWebApi/RestrictedUacAttribute.cs
+++ src/WebApi/practices/04_http_message_handling/src/SampleWebApi/RestrictedUacAttribute.cs
@@ -5,7 +5,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Web.Http.Filters;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
+using SampleWebApi.Services;
 
 namespace SampleWebApi
 {
@@ -13,11 +13,11 @@ namespace SampleWebApi
      * A RestrictedUacAttribute is a filter to eliminate sensitive information to
      * the client. A resource contains management information that is represented
      * by a collection of links. These links will be represented as a array of
-     * objects in JSON. And each object must contains an attribute called 
+     * objects in JSON. And each object must contains an attribute called
      * "restricted". If it is true, then it should be eliminated if the client
      * is a normal user. If it is false, then the information can be seen by both
      * normal user and administrators.
-     * 
+     *
      * NOTE. You are free to add non-public members or methods in the class.
      */
     public class RestrictedUacAttribute : ActionFilterAttribute
@@ -28,10 +28,10 @@ namespace SampleWebApi
 
         /*
          * The attribute takes an argument of the name of the userId parameter in
-         * the route. For example, if the request route definition is 
-         * 
+         * the route. For example, if the request route definition is
+         *
          * http://www.base.com/user/{userId}/resource/type
-         * 
+         *
          * Then the userId parameter name in the route is "userId". The attribute
          * will try resolving the parameter and determine the role of the user by
          * passing the parameter to a RoleRepository. And that is why we ask for
@@ -46,21 +46,23 @@ namespace SampleWebApi
          * The action filter for ASP.NET web API is async nativelly. So we simply
          * abandon the sync version of OnActionExecuted, instead, we will implement
          * the async version directly.
-         * 
+         *
          * Please carefully implement the method to pass all the tests.
          */
         public override async Task OnActionExecutedAsync(
             HttpActionExecutedContext context,
             CancellationToken token)
         {
-           
-            var userId = context.ActionContext.ActionArguments.Where(kv => kv.Key == userIdArgumentName);
+
+            var userId = context.ActionContext.ActionArguments.Where(kv => kv.Key == userIdArgumentName)
+                .Select(kv => (long) kv.Value).ToList();
             if (!userId.Any())
             {
                 context.Response = context.Request.CreateResponse(HttpStatusCode.BadRequest);
                 return;
             }
-            if (userId.First().Value.ToString() == "100")
+
+            if (!context.Response.IsSuccessStatusCode)
             {
                 return;
             }
@@ -70,33 +72,15 @@ namespace SampleWebApi
             {
                 return;
             }
-            var content = await responseContent.ReadAsStringAsync();
-            dynamic result = JsonConvert.DeserializeObject(content);
-            dynamic type = result.type;
-            if (type == "Failed")
-            {
-                return;
-            }
+            dynamic result = JsonConvert.DeserializeObject(await responseContent.ReadAsStringAsync());
 
-            JArray originLinks = result.links;
-            if (originLinks == null)
+            RestrictedUacContractService restrictedUacContractService =
+                (RestrictedUacContractService)context.Request.GetDependencyScope()
+                .GetService(typeof(RestrictedUacContractService));
+            if (restrictedUacContractService.RemoveRestrictedInfo(userId.First(), result))
             {
-                return;
+                context.Response.Content = new StringContent(JsonConvert.SerializeObject(result));
             }
-
-            var links = new JArray();
-            foreach (JToken originLink in originLinks)
-            {
-                dynamic link = originLink;
-                bool? restricted = link.restricted;
-                if (restricted.HasValue && restricted.Value)
-                {
-                    continue;
-                }
-                links.Add(originLink);
-            }
-            result.links = links;
-            context.Response.Content = new StringContent(JsonConvert.SerializeObject(result));
         }
 
         #endregion
